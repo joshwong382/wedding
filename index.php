edbb33f0ea36910c9231867590e4ec6bef61af2e
k2 <kevinwto@hotmail.com>
2025-08-20 15:22:13 -0400

    Test changes for jpg. and small text changes

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 
 $FF_PAGE_ENABLED = true;
 
-$name_groom = "Colin";
-$name_bride = "Chloe";
-$img_carousel1 = "cc-bkgnd.jpg";
+$name_groom = "Test1";
+$name_bride = "Test2";
+$img_carousel1 = "20240914_114409_cori_carrying.jpg";
 $img_menu_cover = "menu-cover.png";
 $img_menu_bot = "menu-bot.png";
 
 $support_name = "Joshua Wong";
 $support_email = "joshua@josh-wong.net";
-$support_domain = "https://wedding.josh-wong.net";
+$support_domain = "https://wedding.josh-wong.net/test";
 
 ?>
 
